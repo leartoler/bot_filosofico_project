2188f974fa69fcd0e280235cb5a5cd1f2ea110d8
8pus <rodrx20@gmail.com>
2021-07-17 13:33:51 -0500

    adding nltk y regex on author

--- tesisBot.py
+++ tesisBot.py
@@ -1,4 +1,5 @@
-import requests, json
+import requests, json, tweepy, nltk, re
+from botFase1 import *
 from random import randint
 
 #hacemos una petición a la api via http y guardamos la db en formato json
@@ -6,13 +7,28 @@ api = requests.get('http://tesis.filos.unam.mx/api/theses')
 api = api.json()
 total = api['total']
 tesis_db = api['data']
+#definimos funcion para imprimir tesis al azar
+def randomThesis():
+#tomamos una tesis al azar y guardamos algunos campos
+    rand = randint(1, len(tesis_db))
+    tesisRandom = tesis_db[rand]
+    keywords = tesisRandom['keywords']
+    url = tesisRandom['rawData']['file']
+    author = tesisRandom['rawData']['author']
+    author = nltk.tokenize.word_tokenize(author)
+    author = [i for i in author if len(i) > 1][::-1]
+    author = re.sub("\W"," ",str(author))
+    author = "".join(author)
+    author = re.sub("[  ]+"," ",author)
+    title = tesisRandom['rawData']['title']
+    year = tesisRandom['rawData']['year']
+    grade = tesisRandom['rawData']['fields']['Grado']
+    text = f"Tesis del año {year} titulada \"{title}\", escrita por{author}para obtener el grado en {grade}. La puedes consultar en {url}"
+    return text
 
-rand = randint(1, len(tesis_db))
-tesisRandom = tesis_db[rand]
-keywords = tesisRandom['keywords']
-url = tesisRandom['rawData']['file']
-author = tesisRandom['rawData']['author']
-title = tesisRandom['rawData']['title']
-year = tesisRandom['rawData']['year']
 
-print(f"Tesis del año {year} titulada \"{title}\", escrita por {author}. la puedes consultar en {url}")
+if __name__ == '__main__':
+    bot = twitter_setup()
+    tesis = randomThesis()
+    bot.update_status(tesis)
+    print(f'Tuit enviado: {tesis}')
